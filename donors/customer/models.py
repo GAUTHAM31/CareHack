6ee2acbd7ce7022bbd7a1830babce34f13b0cad6
Ubuntu <ubuntu@ip-172-31-3-246.ap-south-1.compute.internal>
2017-11-25 11:28:58 +0000

    setup basic django environment

--- donors/customer/models.py
+++ donors/customer/models.py
@@ -8,7 +8,7 @@ class donor(models.Model):
 		return unicode(self.user)
 	user=models.OneToOneField(User, on_delete=models.CASCADE)
 	did=models.IntegerField()
-    fname=models.CharField(max_length=20)
+	fname=models.CharField(max_length=20)
 	lname=models.CharField(max_length=20)
 	age=models.IntegerField()
 	longitude = models.DecimalField(max_digits=8, decimal_places=3)
@@ -16,6 +16,7 @@ class donor(models.Model):
 	credits=models.IntegerField()
 	ld=models.DateField()
 	bt=models.CharField(max_length=10)
+	phno=models.IntegerField(max_length=10)
 	
 class companies(models.Model):
 	cid=models.IntegerField()
--- donors/customer/views.py
+++ donors/customer/views.py
@@ -3,4 +3,42 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from customer.models import donor
+from django.contrib.auth import authenticate, login
+from django.template import RequestContext
+from django.contrib.auth.models import User
+from django.http import Http404, HttpResponseRedirect, HttpResponse
+
+from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
+
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.contrib.auth import logout
+
+def index(request):
+	if request.user.is_authenticated():
+		return redirect(home)
+	else:
+		return render(request,'',{})
+
+
+def logina(request):
+	context = RequestContext(request)
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username,password=password)
+		try:
+			empl=employee.objects.get(user=user)
+		except employee.DoesNotExist:
+			empl=None
+		if empl:
+			login(request,user)
+		else:
+			#HttpResponse("invaild")
+			return redirect(tryagain)#redirect to invaild  username password url
+	else:
+		return redirect(index)#redirect if not post request was send
+def tryagain(request):
+	return render(request,'',{})
 # Create your views here.
